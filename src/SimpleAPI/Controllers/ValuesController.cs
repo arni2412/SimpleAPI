cc6120415f5fba7445228696a7296e097416f301
unknown <arnimasingh24@gmail.com>
2024-07-28 03:05:41 +0530

    dd

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -1,37 +1,30 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SimpleAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ValuesController : ControllerBase
+   public class ValuesController : ControllerBase
     {
         // GET: api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
-            var values = new string[] { "pradeep1", "amar" };
-            return Ok(values); // Return an HTTP 200 response with the list of strings
+             var values = new string[] { "pradeep1", "amar" };
+            return (values); // Return an HTTP 200 response with the list of strings
+       
         }
 
-        // // GET: api/values/5
-        // [HttpGet("{id}")]
-        // public ActionResult<string> Get(int id)
-        // {
-        //     // Simple logic to return a specific value based on ID
-        //     var values = new Dictionary<int, string>
-        //     {
-        //         { 1, "pradeep kumar" },
-        //         { 2, "amar" }
-        //     };
+        //GET: api/values/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "pradeep kumar";
+        }
 
-        //     if (values.TryGetValue(id, out var value))
-        //     {
-        //         return Ok(value); // Return an HTTP 200 response with the string value
-        //     }
-
-        //     return NotFound(); // Return HTTP 404 if the ID is not found
-        // }
     }
 }
