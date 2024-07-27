9fd0097e6d19ebb3e246ca2104c1180cb536bd76
unknown <arnimasingh24@gmail.com>
2024-07-28 02:51:38 +0530

    add value controller

--- src/SimpleAPI/obj/Debug/net7.0/SimpleAPI.AssemblyInfo.cs
+++ src/SimpleAPI/obj/Debug/net7.0/SimpleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a684349707540dae8adc3064d0fb56065fdae285")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c2f7885b2cfb783ea2e5ef028eb5650ef810b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
